e408b3776247bbfb526633303057645c7ce9d081
Christoph Ahlers <c.ahlers@gmx.net>
2018-03-25 17:56:59 +0200

    test: junit5 and mockito

--- tools/interfaceBuilder/src/test/java/application/projects/ProjectServiceTest.java
+++ tools/interfaceBuilder/src/test/java/application/projects/ProjectServiceTest.java
@@ -1,16 +1,55 @@
 package application.projects;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import application.projects.enums.Game;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import unittest.mockito.MockitoExtension;
 
-@RunWith (SpringRunner.class)
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith (MockitoExtension.class)
 @SpringBootTest
 public class ProjectServiceTest {
+	@Mock
+	ProjectJpaRepository projectRepoMock;
+	
+	@InjectMocks
+	private ProjectService projectService;
+	
+	/** Initialize Mockito's mocks **/
+	@BeforeEach
+	public void initMocks() {
+		MockitoAnnotations.initMocks(this);
+	}
 	
 	@Test
-	public void contextLoads() {
+	public void testGetAllProjects() {
+		final Project project1 = new Project("name1", "path1", Game.SC2);
+		project1.setId(1);
+		final Project project2 = new Project("name2", "path2", Game.HEROES);
+		project2.setId(2);
+		when(projectRepoMock.findAll()).thenReturn(Arrays.asList(project1, project2));
+		
+		final int numberOfProjects = projectService.getAllProjects().size();
+		assertEquals(2, numberOfProjects);
+	}
+	
+	@Test
+	public void testSaveProject() {
+		final Project project = new Project("name", "path", Game.SC2);
+		project.setId(1);
+		when(projectRepoMock.save(project)).thenReturn(project);
+		
+		final Project savedProject = projectService.saveProject(project);
+		assertEquals(project, savedProject);
 	}
 	
 }
