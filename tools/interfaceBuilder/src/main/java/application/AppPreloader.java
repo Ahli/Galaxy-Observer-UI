a1def98ae9507b6f300fe501413deb71072acd90
Christoph Ahlers <c.ahlers@gmx.net>
2018-04-04 15:17:27 +0200

    tools: preloader look

--- tools/interfaceBuilder/src/main/java/application/AppPreloader.java
+++ tools/interfaceBuilder/src/main/java/application/AppPreloader.java
@@ -4,18 +4,30 @@ import javafx.application.Preloader;
 import javafx.scene.Scene;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.net.URISyntaxException;
 
 public class AppPreloader extends Preloader {
-	
+	private static final Logger logger = LogManager.getLogger();
 	private Stage stage;
 	
 	@Override
 	public void start(final Stage stage) {
 		this.stage = stage;
-		final Scene scene = new Scene(new ProgressIndicator(-1), 100, 100);
+		final ProgressIndicator progressIndicator = new ProgressIndicator(-1);
+		final Scene scene = new Scene(progressIndicator, 90, 90);
 		stage.initStyle(StageStyle.TRANSPARENT);
+		scene.setFill(Color.TRANSPARENT);
+		try {
+			scene.getStylesheets().add(getClass().getResource("/view/preload.css").toURI().toString());
+		} catch (final URISyntaxException e) {
+			logger.error("preloader stylesheet loading error", e);
+		}
 		stage.setScene(scene);
 		stage.setTitle("Interface Builder");
 		stage.getIcons().add(new Image(getClass().getResourceAsStream("/res/ahli.png"))); //$NON-NLS-1$
