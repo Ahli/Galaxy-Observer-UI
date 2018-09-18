76ec7d44821298ddd23a0248200806d4cd1ffcd7
Christoph Ahlers <c.ahlers@gmx.net>
2018-09-18 19:58:40 +0200

    tools: update

---
+++ tools/observerUiSettingsEditor/project/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module ObserverUiSettingsEditor {
+	requires java.datatransfer;
+	requires java.desktop;
+	requires java.xml;
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires org.apache.commons.io;
+	requires org.apache.commons.lang3;
+	requires org.apache.logging.log4j;
+	requires GalaxyLib;
+}
