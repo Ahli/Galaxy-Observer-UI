d75cd31ce9dff25647c037d9499c6725cf6b62b4
Christoph Ahlers <c.ahlers@gmx.net>
2021-06-03 13:49:09 +0200

    tools: check code with pvs studio

--- tools/galaxyLibrary/project/GalaxyLib/src/test/java/com/ahli/galaxy/ui/abstracts/UIElementTest.java
+++ tools/galaxyLibrary/project/GalaxyLib/src/test/java/com/ahli/galaxy/ui/abstracts/UIElementTest.java
@@ -1,3 +1,6 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
 package com.ahli.galaxy.ui.abstracts;
 
 import com.ahli.galaxy.ui.UIAnimationMutable;
