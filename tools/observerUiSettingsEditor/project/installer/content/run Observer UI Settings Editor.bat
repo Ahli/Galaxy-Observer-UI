d151e2b19e57134702539b81216300357c2a243f
Christoph Ahlers <c.ahlers@gmx.net>
2021-07-27 23:46:57 +0200

    tools: dependency update, disable builder's java-modules as it is causing issues when started within IntelliJ

--- tools/observerUiSettingsEditor/project/installer/content/run Observer UI Settings Editor.bat
+++ tools/observerUiSettingsEditor/project/installer/content/run Observer UI Settings Editor.bat
@@ -3,4 +3,4 @@
 : author: @AhliSC2
 
 : use "vm\bin\java.exe" if the App is failing to start to see error info
-start /MAX "" "vm\bin\java.exe" -m ObserverUiSettingsEditor/com.ahli.hotkey_ui.application.Main
+start /MAX "" "vm\bin\javaw.exe" -m ObserverUiSettingsEditor/com.ahli.hotkey_ui.application.Main
