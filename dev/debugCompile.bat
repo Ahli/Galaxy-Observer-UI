74d6a360669d3fc4e397a481966f969c127c64f3
Christoph Ahlers <c.ahlers@gmx.net>
2021-09-17 01:16:23 +0200

    tools: experiment with multiple GC algorithms, use a low latency one as all tested perform equally good and low latency is good for UI

--- dev/debugCompile.bat
+++ dev/debugCompile.bat
@@ -2,7 +2,18 @@
 : Batch Script 
 : author: @AhliSC2
 
-java -jar compile-spring-boot.jar -XX:G1PeriodicGCInterval=5000 -XX:G1PeriodicGCSystemLoadThreshold=0.1
+: G1GC
+: java -jar compile-spring-boot.jar -XX:G1PeriodicGCInterval=5000 -XX:G1PeriodicGCSystemLoadThreshold=0.1
+
+: ZGC
+: java -jar compile-spring-boot.jar -XX:+UseZGC -XX:ZUncommitDelay=5
+
+: ShenandoahGC
+java -jar compile-spring-boot.jar -XX:+UseShenandoahGC
+
+: ParallelGC
+: java -jar compile-spring-boot.jar -XX:+UseParallelGC
+
 
 : line breaks
 echo.
