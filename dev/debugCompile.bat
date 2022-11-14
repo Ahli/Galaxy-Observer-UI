8937fef8ec68e2a699bd6dfce807f95255d0b152
Christoph Ahlers <c.ahlers@gmx.net>
2022-11-14 16:31:57 +0100

    tools: use ZGC, update dependencies

--- dev/debugCompile.bat
+++ dev/debugCompile.bat
@@ -5,11 +5,11 @@
 : G1GC
 : java -jar compile-spring-boot.jar -XX:G1PeriodicGCInterval=5000 -XX:G1PeriodicGCSystemLoadThreshold=0.1
 
-: ZGC
-: java -jar compile-spring-boot.jar -XX:+UseZGC -XX:ZUncommitDelay=5
+: ZGC (works without my GC hacks)
+java -jar compile-spring-boot.jar -XX:+UseZGC
 
 : ShenandoahGC
-java -jar compile-spring-boot.jar -XX:+UseShenandoahGC
+: java -jar compile-spring-boot.jar -XX:+UseShenandoahGC
 
 : ParallelGC
 : java -jar compile-spring-boot.jar -XX:+UseParallelGC
