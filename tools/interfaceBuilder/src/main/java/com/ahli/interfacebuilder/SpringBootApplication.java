5c12ce6d0eb1ae02b10f7cc6b8f8615e4341ac8c
Christoph Ahlers <Ahli@users.noreply.github.com>
2024-01-27 12:50:57 +0100

    tools: move h2 migration to own class

--- tools/interfaceBuilder/src/main/java/com/ahli/interfacebuilder/SpringBootApplication.java
+++ tools/interfaceBuilder/src/main/java/com/ahli/interfacebuilder/SpringBootApplication.java
@@ -23,17 +23,12 @@ import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfigu
 import org.springframework.context.annotation.Import;
 import org.springframework.lang.Nullable;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URI;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 
+import static com.ahli.interfacebuilder.integration.h2.H2Integration.migrateH2Db;
 import static com.ahli.interfacebuilder.ui.AppController.FATAL_ERROR;
 
 // start with VM parameter if java modules are used: -add-opens=javafx.controls/javafx.scene.control=interfacex.builder
@@ -250,13 +245,10 @@ import static com.ahli.interfacebuilder.ui.AppController.FATAL_ERROR;
 				"org.springframework.boot.autoconfigure.web.reactive.WebSessionIdResolverAutoConfiguration",
 				"org.springframework.boot.autoconfigure.websocket.servlet.WebSocketMessagingAutoConfiguration",
 				"org.springframework.boot.autoconfigure.websocket.reactive.WebSocketReactiveAutoConfiguration",
-				"org.springframework.boot.autoconfigure.websocket.servlet.WebSocketServletAutoConfiguration"}
-)
+				"org.springframework.boot.autoconfigure.websocket.servlet.WebSocketServletAutoConfiguration" })
 public final class SpringBootApplication {
 	
 	public static final int INTER_PROCESS_COMMUNICATION_PORT = 12317;
-	public static final String DB_FILE_NAME = "DB.mv.db";
-	public static final String DB_TRACE_FILE_NAME = "DB.trace.db";
 	private static final boolean USE_DOMAIN_SOCKET = true;
 	
 	public SpringBootApplication() {
@@ -342,79 +334,6 @@ public final class SpringBootApplication {
 		} else {
 			launchGui(args, serverThread);
 		}
-	}
-	
-	/**
-	 * Writes exportMigrate.zip and deletes the database files. The zip archive should be loaded by the DB migration.
-	 *
-	 * @throws IOException
-	 * @throws InterruptedException
-	 */
-	private static void migrateH2Db() throws IOException, InterruptedException {
-		final Path dbDir = getDbDir();
-		int oldVersion = oldH2DbFilePresent();
-		switch (oldVersion) {
-			case 1 -> {
-				backupH2Db("1.4.200", dbDir);
-				Files.deleteIfExists(dbDir.resolve(DB_FILE_NAME));
-				Files.deleteIfExists(dbDir.resolve(DB_TRACE_FILE_NAME));
-			}
-			case 2 -> {
-				backupH2Db("2.1.214", dbDir);
-				Files.deleteIfExists(dbDir.resolve(DB_FILE_NAME));
-				Files.deleteIfExists(dbDir.resolve(DB_TRACE_FILE_NAME));
-			}
-			default -> Files.deleteIfExists(dbDir.resolve("exportMigrate.zip"));
-		}
-	}
-	
-	private static int oldH2DbFilePresent() {
-		final Path dbPath = getDbDir().resolve(DB_FILE_NAME);
-		if (Files.exists(dbPath)) {
-			try (final BufferedReader brTest = Files.newBufferedReader(dbPath)) {
-				final String firstLine = brTest.readLine();
-				if (firstLine.contains(",format:1,")) {
-					return 1;
-				}
-				if (firstLine.contains(",format:2,")) {
-					return 2;
-				}
-			} catch (final IOException e) {
-				log.error("Failed to read H2 DB file to determine version.");
-			}
-		}
-		return 0;
-	}
-	
-	private static Path getDbDir() {
-		return Path.of(System.getProperty("user.home"), ".GalaxyObsUI", "database");
-	}
-	
-	private static void backupH2Db(final String h2Version, final Path dbDir) throws IOException, InterruptedException {
-		final Path h2JarFile = dbDir.resolve("h2-" + h2Version + ".jar");
-		if (!Files.exists(h2JarFile)) {
-			// download e.g. https://repo1.maven.org/maven2/com/h2database/h2/1.4.200/h2-1.4.200.jar
-			final URI uri = URI.create(
-					"https://repo1.maven.org/maven2/com/h2database/h2/" + h2Version + "/h2-" + h2Version + ".jar");
-			try (final ReadableByteChannel readableByteChannel = Channels.newChannel(uri.toURL().openStream());
-			     final FileOutputStream fileOutputStream = new FileOutputStream(h2JarFile.toFile())) {
-				fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
-			}
-		}
-		
-		// java -cp "h2-1.4.200.jar" org.h2.tools.Script -url jdbc:h2:~/test -user sa -script test.zip -options compression zip
-		final String exportZipFile = getDbDir() + File.separator + "exportMigrate.zip";
-		
-		final String[] cmd = new String[] { "java", "-cp", h2JarFile.toString(), "org.h2.tools.Script", "-url",
-				"jdbc:h2:file:~/.GalaxyObsUI/database/DB", "-user", "sa", /*"-password", "",*/ "-script", exportZipFile,
-				"-options", "compression", "zip" };
-		
-		if (log.isTraceEnabled()) {
-			log.trace("executing: {}", Arrays.toString(cmd));
-		}
-		Runtime.getRuntime().exec(cmd).waitFor();
-		
-		Files.deleteIfExists(h2JarFile);
 	}
 	
 	private static void launchNoGui(final String[] args, @Nullable final IpcServerThread serverThread) {
